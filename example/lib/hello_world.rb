78465fa03e585b048d755847140c34c71dceb692
7rans <transfire@gmail.com>
2011-05-14 17:39:47 -0400

    admin: add example

---
+++ example/lib/hello_world.rb
@@ -0,0 +1,13 @@
+# This is a class.
+class HelloWorld
+
+  # Hello simply prints out a greeting.
+  #
+  # string - Object that responds to #to_s
+  #
+  # Return nothing.
+  def hello(string="World)
+    puts "Hello #{string}"
+  end
+
+end
