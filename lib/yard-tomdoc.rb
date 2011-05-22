3f970b01fa8cfdedaa8992487668af441255be7e
7rans <transfire@gmail.com>
2011-05-22 11:28:39 -0400

    use actual tomdoc dependency

--- lib/yard-tomdoc.rb
+++ lib/yard-tomdoc.rb
@@ -1,4 +1,5 @@
-require File.dirname(__FILE__) + "/yard-tomdoc/tomdoc"  #require 'tomdoc/tomdoc'
+#require File.dirname(__FILE__) + "/yard-tomdoc/tomdoc"  #require 'tomdoc/tomdoc'
+require 'tomdoc/tomdoc'
 
 module YARD
 
