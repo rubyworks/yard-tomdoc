86b1203dd275e83bc340129f14ab7a3eaf8fc969
7rans <transfire@gmail.com>
2013-02-15 20:49:21 -0500

    Bump to version 0.7.1. [admin]

--- work/sandbox/example.rb
+++ work/sandbox/example.rb
@@ -1,17 +1,21 @@
 class Example
-  # Public: Duplicate some text an arbitrary number of times.
+  # Public: Duplicate some text an arbitrary number of times. [String]
   #
   # text  - The String to be duplicated.
   #         What if it has multiple lines?
   # count - The Integer number of times to duplicate the text.
   #         This one could too.
   #
-  # Examples
+  # Example
   #
-  #   multiplex('Tom', 4)
-  #   # => 'TomTomTomTom'
+  #   class Child
+  #     extend AccountInheritance
   #
-  # Returns the duplicated String.
+  #     belongs_to :parent
+  #
+  #     inherits_account_from :parent
+  #   end
+  #
   def multiplex(text, count)
     text * count
   end
